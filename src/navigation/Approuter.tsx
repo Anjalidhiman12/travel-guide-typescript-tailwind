fc9ee1ae634fc40e0fb11f939324d9b2cb745da1
Gautham-279 <gautam.shetty@exathought.com>
2023-09-11 19:00:10 +0530

    changes

--- src/navigation/Approuter.tsx
+++ src/navigation/Approuter.tsx
@@ -4,12 +4,13 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Page1 from '../pages/page1';
 import Page2 from '../pages/page2';
-import Page3 from "../pages/page3"
+import Page3 from "../pages/page3";
+import WelcomeScreen from '../pages/landing-page/welcome-screen';
 const AppRouter=(props:any)=>(
        <Router>
 
              <Routes>
-             <Route  path='/' element={< Page1 {...props}/>}></Route>
+             <Route  path='/' element={< WelcomeScreen {...props}/>}></Route>
 
                  <Route  path='/page1' element={< Page1  {...props}/>}></Route>
                  <Route  path='/page2' element={< Page2 {...props}/>}></Route>
---
+++ src/pages/landing-page/welcome-screen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const WelcomeScreen=(props:any)=>{
+
+
+
+    return(
+        <div className='w-20px h-20px bg-red'>
+
+        Hello  JAY
+        </div>
+    )
+}
+
+export default WelcomeScreen;
+
