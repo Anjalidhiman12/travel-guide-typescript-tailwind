12711569f1b88efe9711353d964c5e64ddef4376
Anjali Dhiman <anjali.dhiman@exathoight.com>
2023-09-13 19:20:48 +0530

    Initial start of tailwind

--- src/pages/landing-page/welcome-screen.tsx
+++ src/pages/landing-page/welcome-screen.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import AppRouter from "../../navigation/Approuter";
 
-
-const WelcomeScreen=(props:any)=>{
-
-
-
-    return(
-        <div className='w-20px h-20px bg-red'>
-
-        Hello  JAY
-        </div>
-    )
-}
+const WelcomeScreen = (props: any) => {
+  return (
+    <div className="h-screen w-screen bg-red-500 flex items-center justify-center">
+      <h1 className="">Welcome To travel guide</h1>
+      <Link id="link" to={"/page1"}>
+        <h1>Click here to start</h1>
+      </Link>
+    </div>
+  );
+};
 
 export default WelcomeScreen;
-
